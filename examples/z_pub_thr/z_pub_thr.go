7721618b7fc32a9369dc9047f350735977c76edb
Julien Enoch <946462+JEnoch@users.noreply.github.com>
2019-08-23 16:51:51 +0200

    Add examples z_pub_thr and z_sub_thr

---
+++ examples/z_pub_thr/z_pub_thr.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/atolab/zenoh-go"
+)
+
+func main() {
+	locator := "tcp/127.0.0.1:7447"
+	if len(os.Args) < 2 {
+		fmt.Printf("USAGE:\n\tz_pub_thr <payload-size> [<zenoh-locator>]\n\n")
+		os.Exit(-1)
+	}
+
+	length, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Running throughput test for payload of %d bytes\n", length)
+	if len(os.Args) > 2 {
+		locator = os.Args[2]
+	}
+
+	vle := zenoh.VleEncode(length)
+	data := make([]byte, length)
+	for i := 0; i < length; i++ {
+		data[i] = byte(i % 10)
+	}
+	data = append(vle, data...)
+
+	z, err := zenoh.ZOpen(locator)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pub, err := z.DeclarePublisher("/test/thr")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for true {
+		pub.StreamData(data)
+	}
+
+}
---
+++ examples/z_sub_thr/z_sub_thr.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/atolab/zenoh-go"
+)
+
+const n = 100000
+
+var count uint64
+var start, stop time.Time
+
+func printStats(start time.Time, stop time.Time) {
+	t0 := float64(start.UnixNano()) / 1000000000.0
+	t1 := float64(stop.UnixNano()) / 1000000000
+	thpt := float64(n) / (t1 - t0)
+	fmt.Printf("%f msgs/sec\n", thpt)
+}
+
+func listener(rid string, data []byte, info *zenoh.DataInfo) {
+	if count == 0 {
+		start = time.Now()
+		count++
+	} else if count < n {
+		count++
+	} else {
+		fmt.Println()
+		stop = time.Now()
+		printStats(start, stop)
+		count = 0
+	}
+}
+
+func main() {
+	locator := "tcp/127.0.0.1:7447"
+	if len(os.Args) > 1 {
+		locator = os.Args[1]
+	}
+
+	fmt.Printf("Connecting to %s...\n", locator)
+	z, err := zenoh.ZOpen(locator)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("Declaring Subscriber...\n")
+	_, err = z.DeclareSubscriber("/test/thr", zenoh.NewSubMode(zenoh.ZPushMode), listener)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	time.Sleep(60 * time.Second)
+
+}
